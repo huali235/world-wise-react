b20eca021a81ae104c0ffc47af73be57822038e2
Hussain Ali <huali235@gmail.com>
2023-11-11 21:01:50 -0500

    changed imports

--- world-wise/src/pages/AppLayout/AppLayout.jsx
+++ world-wise/src/pages/AppLayout/AppLayout.jsx
@@ -1,9 +1,12 @@
-import AppNav from "../../components/AppNav/AppNav.jsx";
+import Map from "../../components/Map/Map.jsx";
+import Sidebar from "../../components/Sidebar/Sidebar.jsx";
+import styles from "./AppLayout.module.css";
 
 function AppLayout() {
   return (
-    <div>
-      <AppNav />
+    <div className={styles.app}>
+      <Sidebar />
+      <Map />
     </div>
   );
 }
